634cf125b7cd448a68c4539af60286d139d9ae94
bhaveshkhatri81 <69708924+bhaveshkhatri81@users.noreply.github.com>
2023-10-24 03:17:07 +0530

    Update contributorsList.js
    Added my name in the list

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+    {
+    id: 426,
+    fullname: "Bhavesh Khatri",
+    username: "https://github.com/bhaveshkhatri81/",
+  },
 ];
